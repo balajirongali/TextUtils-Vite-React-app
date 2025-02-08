bb56651961dc8267d9fc30884e0f6c8bb520ef61
balajirongali <balajirongali0504@gmail.com>
2025-02-08 15:15:09 +0530

    removed vite.svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm'
 import Alert from './components/Alert'
